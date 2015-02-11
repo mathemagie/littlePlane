511a6752f5fc508b45d270489631bf58d214e197
Fache Aurélien <aurelien.fache@gmail.com>
2015-02-11 14:39:43 +0100

    Create get_status_flight.php

---
+++ get_status_flight.php
@@ -0,0 +1,14 @@
+<?php
+                $id = '5686558';
+                $id = '57cccb2';
+                $url = 'http://krk.fr24.com/_external/planedata_json.1.3.php?f=' . $id;
+        $json = file_get_contents($url);
+        $data = json_decode($json,true);
+        $status = $data['status'];
+        echo $status;
+        if ($status == 'landed') echo "1";
+        if ($status == 'airborne') echo "2";
+
+        //print_r(array("lat" => $data['trail'][0], 'lng' => $data['trail'][1]));
+        //echo "1";
+ ?>
