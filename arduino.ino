a948f22544ec07bca3457f31e9177c8d783b6e17
Fache Aurélien <aurelien.fache@gmail.com>
2015-02-16 16:29:08 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -4,7 +4,11 @@
 
 const int ledPin = 13; 
 
+String readString;
+
 Servo monServo;
+int is_on_water;
+int flight_status;
 
 void setup() {
   Bridge.begin();   // Initialize the Bridge
@@ -19,7 +23,7 @@ void loop() {
   // This command line runs the WifiStatus script, (/usr/bin/pretty-wifi-info.lua), then 
   // sends the result to the grep command to look for a line containing the word
   // "Signal:"  the result is passed to this sketch:
-  p.runShellCommand("/usr/bin/curl http://mathemagie.net/projects/flight/index.php");
+  p.runShellCommand("/usr/bin/curl http://mathemagie.net/littleplane/get_flight_status.php?team_id=1");
   Serial.println("run curl");
 
   // do nothing until the process finishes, so you get the whole output:
@@ -27,9 +31,17 @@ void loop() {
 
   // Read command output. runShellCommand() should have passed "Signal: xx&":
   while (p.available()) {
-    int result = p.parseInt();  
-    Serial.println(result);
-    if (result) {
+    char c  =  p.read();
+    readString += c;
+  }
+  
+  if (readString.length() >0) {
+  
+    //Serial.print(readString);
+    flight_status = readString.substring(0,1).toInt();
+    is_on_water = readString.substring(2,3).toInt();
+    if (is_on_water) {
+      Serial.println("flight on water");
       digitalWrite(ledPin, HIGH);
        monServo.write(90);
       delay(1000);      
@@ -38,11 +50,16 @@ void loop() {
         monServo.write(180);
        delay(1000);      
     }
- 
-
-    // look for an integer
+    if (flight_status) {
+      Serial.println("flight airborad");
+        monServo.write(90);
+      delay(1000);      
+    }else {
+       monServo.write(180);
+       delay(1000);      
+    }
+   readString=""; 
+   }
    
-  
-  } 
   delay(1000);  // wait 5 seconds before you do it again
 }
