4472ec3a1ca9f0f66d5eb842e45d3d0bd1c448e0
cinematica <cinematica@users.noreply.github.com>
2015-02-26 18:35:45 +0100

    Update knob.ino
    Change of comments
    Change of variable names from potpin to LDR_pin

--- knob.ino
+++ knob.ino
@@ -1,17 +1,19 @@
 /* 
- Controlling a servo position using a potentiometer (variable resistor) 
- by Michal Rinott <http://people.interaction-ivrea.it/m.rinott> 
+  Controlling a servo position using an LDR (Light Dependant Resistor) 
+  Reads the analog input on pin 0, from the LDR.
+  Uses the value red from the LDR to change the position of the servo, connected to pin 9.
+  This example code is a variant from Knob, found in the public domain.
+ 
+  version of March 4th 2015 
+  by Airbus Team XX
+ */
 
- modified on 8 Nov 2013
- by Scott Fitzgerald
- http://arduino.cc/en/Tutorial/Knob
-*/
-
+// We tell Arduino that we will use a library called Servo in this sketch
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 
-int potpin = 0;  // analog pin used to connect the potentiometer
+int LDR_pin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
 void setup()
@@ -21,7 +23,7 @@ void setup()
 
 void loop() 
 { 
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
+  val = analogRead(LDR_pin);            // reads the value of the potentiometer (value between 0 and 1023) 
   val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180) 
   myservo.write(val);                  // sets the servo position according to the scaled value 
   delay(15);                           // waits for the servo to get there 
