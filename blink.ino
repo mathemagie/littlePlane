e0477c9a1a698c8a4151f197184b7fa2f8bec90f
cinematica <cinematica@users.noreply.github.com>
2015-02-26 10:58:42 +0100

    Update blink.ino
    Cristina modified comments for Airbus Workshop

--- blink.ino
+++ blink.ino
@@ -1,21 +1,27 @@
 /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
+  This example code has been copied from the public domain.
 
-  This example code is in the public domain.
+  version of March 4th 2015 
+  by Airbus Team XX
+
  */
 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+// First of all we declare any variables used by the sketch
+// We create a variable called "Led" that contains a number
+// This number is the one of the pin to which we will connect our LED:
 int led = 13;
 
-// the setup routine runs once when you press reset:
+// the setup routine runs once when you press reset or upload a new sketch:
 void setup() {
-  // initialize the digital pin as an output.
+  // we tell arduino to initialize the digital pin number 13 as an output.
+  // note that OUTPUT is in blue, this is called a "constant" it has a value
+  // that is pre-defined in arduino so the IDE recognises it
   pinMode(led, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
+// the loop routine runs over and over when the arduino is powered:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
