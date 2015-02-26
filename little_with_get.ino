18ba80f5bc28f73047ca01b4f42d74440b8d7572
Fache Aurélien <aurelien.fache@gmail.com>
2015-02-26 18:35:18 +0100

    Update little_with_get.ino

--- little_with_get.ino
+++ little_with_get.ino
@@ -5,6 +5,9 @@
 const int ledPinSea = 13; 
 const int ledPinGround = 12; 
 
+const int plane_flying = 2;
+const int plane_landed = 1;
+
 String readString;
 
 Servo monServo;
@@ -22,52 +25,48 @@ void setup() {
 }
 
 void loop() {
-   HttpClient client;
   
-   
- client.get("http://mathemagie.net/littleplane/get_flight_status.php?team_id=1");
+  HttpClient client;
+  client.get("http://mathemagie.net/littleplane/get_flight_status.php?team_id=1");
+  
   Serial.println("run httpget");
-   while (client.available()) {
-    char c = client.read();
   
-     readString += c;
+  while (client.available()) {
+    char c = client.read();
+    readString += c;
   }
-    Serial.println(readString);
+  Serial.println(readString);
      
   if (readString.length() >0) {
   
-    //Serial.print(readString);
     flight_status = readString.substring(0,1).toInt();
     is_on_water = readString.substring(2,3).toInt();
     
     if (is_on_water) {
       Serial.println("flight on water");
       digitalWrite(ledPinSea, HIGH);
-       digitalWrite(ledPinGround, LOW);
+      digitalWrite(ledPinGround, LOW);
       delay(1000);      
     }
     else {
-       Serial.println("flight on land");
+      Serial.println("flight on land");
       digitalWrite(ledPinGround, HIGH);
       digitalWrite(ledPinSea, LOW);
       delay(1000);      
     }
     
-    if (flight_status == 2) {
+    if (flight_status == plane_flying) {
       Serial.println("flight airborne");
       monServo.write(10);//en haut
       delay(1000);      
     }
     
-    if (flight_status == 1) {
+    if (flight_status == plane_landed) {
       Serial.println("flight landed");
       monServo.write(180);//en bas
       delay(1000);      
     }
    readString=""; 
    }
-  
-
-  
-  delay(1000);  // wait 5 seconds before you do it again
+    delay(1000);  // wait 5 seconds before you do it again
 }
